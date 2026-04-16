9918a81516cd3918ab081bd0aba16057abee36f4
Daniel Lee <mildz79@gmail.com>
2026-04-16 16:37:51 -0700

    Delete supabase/migrations directory

--- supabase/migrations/003_create_storage_bucket.sql
+++
@@ -1,4 +0,0 @@
--- Create private 'financials' storage bucket for P&L xlsx files
-insert into storage.buckets (id, name, public)
-values ('financials', 'financials', false)
-on conflict (id) do nothing;
